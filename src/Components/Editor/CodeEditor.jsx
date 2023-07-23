142b54c5a276f17e5f9971b991954cb00f814867
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-23 19:47:09 +0530

    Dynamic Code Previewer

--- src/Components/Editor/CodeEditor.jsx
+++ src/Components/Editor/CodeEditor.jsx
@@ -1,87 +1,3 @@
-// import React, { useState } from 'react';
-
-// const CodeEditor = () => {
-//   const [htmlCode, setHtmlCode] = useState('');
-//   const [cssCode, setCssCode] = useState('');
-//   const [jsCode, setJsCode] = useState('');
-
-//   const handleOutput = () => {
-//     const iframe = document.getElementById('output');
-//     const combinedCode = `
-//       <html>
-//         <head>
-//           <style>${cssCode}</style>
-//         </head>
-//         <body>${htmlCode}</body>
-//         <script>${jsCode}</script>
-//       </html>
-//     `;
-
-//     // Safely execute the JS code in a function scope
-//     try {
-//       const executeCode = new Function(jsCode);
-//       executeCode();
-//     } catch (error) {
-//       console.error(error);
-//     }
-
-//     iframe.contentDocument.body.innerHTML = combinedCode;
-//   };
-
-//   return (
-//     <div id='main-container'>
-//       <div className="box">
-//         <div className="heading">HTML</div>
-//         <textarea
-//           id="htmlFrame"
-//           onChange={(e) => setHtmlCode(e.target.value)}
-//         />
-//       </div>
-
-//       <div className="box">
-//         <div className="heading">CSS</div>
-//         <textarea
-//           id="cssFrame"
-//           onChange={(e) => setCssCode(e.target.value)}
-//         />
-//       </div>
-
-//       <div className="box">
-//         <div className="heading">JS</div>
-//         <textarea
-//           id="jsFrame"
-//           onChange={(e) => setJsCode(e.target.value)}
-//         />
-//       </div>
-
-//       <div className="box">
-//         <div className="heading-and-button">
-//           <div className="heading">Live Preview</div>
-//           <button className="run-button" id="runButton" onClick={handleOutput} >
-//             Run
-//           </button>
-//         </div>
-//         <div className="output-container">
-//           <iframe id="output" title="Live Preview" />
-//         </div>
-
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default CodeEditor
-
-
-
-
-
-
-
-
-
-
 import React, { useState, useRef } from 'react';
 import { SketchPicker } from 'react-color';
 import BackgroundColorPicker from '../BackgroundColorPicker/BackgroundColorPicker';
@@ -97,32 +13,40 @@ const CodeEditor = () => {
   const [showCssColorPicker, setShowCssColorPicker] = useState(false);
   const [showJsColorPicker, setShowJsColorPicker] = useState(false);
 
-  const htmlRef = useRef(null);
-  const cssRef = useRef(null);
-  const jsRef = useRef(null);
+  const htmlRef = useRef();
+  const cssRef = useRef();
+  const jsRef = useRef();
 
-  const handleHtmlColorChange = (color) => {
-    setHtmlBgColor(color.hex);
+  const handleColorChange = (type, color) => {
+    switch (type) {
+      case 'html':
+        setHtmlBgColor(color.hex);
+        break;
+      case 'css':
+        setCssBgColor(color.hex);
+        break;
+      case 'js':
+        setJsBgColor(color.hex);
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleCssColorChange = (color) => {
-    setCssBgColor(color.hex);
-  };
-
-  const handleJsColorChange = (color) => {
-    setJsBgColor(color.hex);
-  };
-
-  const handleHtmlColorPickerClick = () => {
-    setShowHtmlColorPicker(!showHtmlColorPicker);
-  };
-
-  const handleCssColorPickerClick = () => {
-    setShowCssColorPicker(!showCssColorPicker);
-  };
-
-  const handleJsColorPickerClick = () => {
-    setShowJsColorPicker(!showJsColorPicker);
+  const handleColorPickerClick = (type) => {
+    switch (type) {
+      case 'html':
+        setShowHtmlColorPicker(!showHtmlColorPicker);
+        break;
+      case 'css':
+        setShowCssColorPicker(!showCssColorPicker);
+        break;
+      case 'js':
+        setShowJsColorPicker(!showJsColorPicker);
+        break;
+      default:
+        break;
+    }
   };
 
   const handleOutput = () => {
@@ -152,74 +76,61 @@ const CodeEditor = () => {
     iframe.contentDocument.body.innerHTML = '';
   };
 
+  // Function to handle textarea input and update line numbers
+  const handleTextAreaInput = (e, ref) => {
+    const lines = e.target.value.split('\n');
+    ref.current.style.setProperty('--num-lines', lines.length);
+  };
 
   return (
     <div id='main-container'>
-      <div className="box">
-        <div className="heading">
-          HTML
-          <BackgroundColorPicker
-            bgColor={htmlBgColor}
-            onColorChange={handleHtmlColorChange}
-            showColorPicker={showHtmlColorPicker}
-            onColorPickerClick={handleHtmlColorPickerClick}
+      {['html', 'css', 'js'].map((type) => (
+        <div className="box" key={type}>
+          <div className="heading">
+            {type.toUpperCase()}
+            <BackgroundColorPicker
+              bgColor={type === 'html' ? htmlBgColor : type === 'css' ? cssBgColor : jsBgColor}
+              onColorChange={(color) => handleColorChange(type, color)}
+              showColorPicker={
+                type === 'html' ? showHtmlColorPicker : type === 'css' ? showCssColorPicker : showJsColorPicker
+              }
+              onColorPickerClick={() => handleColorPickerClick(type)}
+            />
+          </div>
+          <textarea
+            id={`${type}Frame`}
+            onChange={(e) => {
+              switch (type) {
+                case 'html':
+                  setHtmlCode(e.target.value);
+                  break;
+                case 'css':
+                  setCssCode(e.target.value);
+                  break;
+                case 'js':
+                  setJsCode(e.target.value);
+                  break;
+                default:
+                  break;
+              }
+            }}
+            style={{ background: type === 'html' ? htmlBgColor : type === 'css' ? cssBgColor : jsBgColor }}
+            ref={type === 'html' ? htmlRef : type === 'css' ? cssRef : jsRef}
           />
         </div>
-        <textarea
-          id="htmlFrame"
-          onChange={(e) => setHtmlCode(e.target.value)}
-          style={{ background: htmlBgColor }}
-          ref={htmlRef}
-        />
-      </div>
-
-      <div className="box">
-        <div className="heading">
-          CSS
-          <BackgroundColorPicker
-            bgColor={cssBgColor}
-            onColorChange={handleCssColorChange}
-            showColorPicker={showCssColorPicker}
-            onColorPickerClick={handleCssColorPickerClick}
-          />
-        </div>
-        <textarea
-          id="cssFrame"
-          onChange={(e) => setCssCode(e.target.value)}
-          style={{ background: cssBgColor }}
-          ref={cssRef}
-        />
-      </div>
-
-      <div className="box">
-        <div className="heading">
-          JS
-             <BackgroundColorPicker
-            bgColor={jsBgColor}
-            onColorChange={handleJsColorChange}
-            showColorPicker={showJsColorPicker}
-            onColorPickerClick={handleJsColorPickerClick}
-          />
-        </div>
-        <textarea
-          id="jsFrame"
-          onChange={(e) => setJsCode(e.target.value)}
-          style={{ background: jsBgColor }}
-          ref={jsRef}
-        />
-      </div>
+      ))}
 
       <div className="box">
         <div className="heading-and-button">
           <div className="heading">Live Preview</div>
-         <div id="buttons">
-         <button className="btn" id="runButton" onClick={handleOutput}>
-            Run
-          </button>
-          <button className="btn" onClick={handleClearClick}>
-            Clear
-          </button>
-         </div>
+          <div id="buttons">
+            <button className="btn" id="runButton" onClick={handleOutput}>
+              Run
+            </button>
+            <button className="btn" onClick={handleClearClick}>
+              Clear
+            </button>
+          </div>
         </div>
         <div className="output-container">
           <iframe id="output" title="Live Preview" />
